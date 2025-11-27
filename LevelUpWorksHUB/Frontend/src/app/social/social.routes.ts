c2e2d13448f5d8d0e221cf150f07fc98faaf3329
IEVN1001-22001567 <81333@alumnos.utleon.edu.mx>
2025-11-27 15:52:48 -0600

    Nuevos Modulos, NavBarActualizada

---
+++ LevelUpWorksHUB/Frontend/src/app/social/social.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { SocialComponent } from './social.component';
+
+export const SOCIAL_ROUTES: Routes = [
+  {
+    path: '',
+    component: SocialComponent
+  }
+];
