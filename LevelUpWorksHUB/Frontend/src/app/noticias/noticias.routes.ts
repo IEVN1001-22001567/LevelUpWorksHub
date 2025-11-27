5697aef8b134f830b7e388f67ee1f3d164247f0d
IEVN1001-22001567 <81333@alumnos.utleon.edu.mx>
2025-11-27 13:37:43 -0600

    Psicho,Noticias y Eventos

---
+++ LevelUpWorksHUB/Frontend/src/app/noticias/noticias.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { NoticiasComponent } from './noticias.component';
+
+export const NOTICIAS_ROUTES: Routes = [
+  {
+    path: '',
+    component: NoticiasComponent
+  }
+];
