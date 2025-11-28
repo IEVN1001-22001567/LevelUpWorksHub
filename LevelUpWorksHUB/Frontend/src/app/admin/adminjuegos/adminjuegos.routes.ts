c1eaaf79ebfed5a9e0a20b1266de7883e2ab9c2c
IEVN1001-22001567 <81333@alumnos.utleon.edu.mx>
2025-11-27 21:53:22 -0600

    Modulo Admin Usuarios y Juegos

---
+++ LevelUpWorksHUB/Frontend/src/app/admin/adminjuegos/adminjuegos.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AdminJuegosComponent } from './adminjuegos.component';
+
+export const ADMINJUEGOS_ROUTES: Routes = [
+  {
+    path: '',
+    component: AdminJuegosComponent
+  }
+];
