7450b3e3b9577591c7e27c1bc2f4e07997e88843
IEVN1001-22002352 <80827@alumnos.utleon.edu.mx>
2025-11-27 18:42:27 -0600

    Biblioteca, gestionCompras y carrito

---
+++ LevelUpWorksHUB/Frontend/src/app/carrito/carrito.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CarritoComponent } from './carrito.component';
+
+export const CARRITO_ROUTES: Routes = [
+  {
+    path: '',
+    component: CarritoComponent
+  }
+];
---
+++ LevelUpWorksHUB/Frontend/src/app/gestion-compras/gestion-compras.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { GestionComprasComponent } from './gestion-compras.component';
+
+export const GESTIONCOMPRAS_ROUTES: Routes = [
+  {
+    path: '',
+    component: GestionComprasComponent
+  }
+];
